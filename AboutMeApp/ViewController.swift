910474f32c4c79a583dd2378938fd284eb35c66a
Evgeny Likhachev <EvgenyLi429@gmail.com>
2023-10-08 16:27:48 +0200

    minor updates

--- AboutMeApp/ViewController.swift
+++ AboutMeApp/ViewController.swift
@@ -1,19 +1,56 @@
-//
-//  ViewController.swift
-//  AboutMeApp
-//
-//  Created by Evgeny Likhachev on 7/10/23.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    private let user = "user"
+    private let password = "password"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
-
+    /*peredai imya na welcomeview
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        <#code#>
+    }
+     */
+    
+    @IBAction func loginButton() {
+        // if pressed, checks username and pass
+        // if true = open welcomeview else error.alert
+        guard userNameTextField.text == user && passwordTextField.text == password else {
+            
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard userNameTextField.text == user, passwordTextField.text == password else {
+            return false
+        }
+        return true
+    }
+    
+    @IBAction func forgotUserName() {
+        //alert username
+        UIAlertAction(
+            title: "Ya name is:\(user)", style: UIAlertAction.Style)
+    }
+    
+    @IBAction func fogotPassword() {
+        //alert password
+        UIAlertAction(title: "Ya password is:\(password)")
+    }
+    
+    
+    
+    
 }
 
---
+++ WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeViewController.swift
+//  AboutMeApp
+//
+//  Created by Evgeny Likhachev on 8/10/23.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+}
